221db7fbb48fc6bc40e8268efbc3c06d611594df
jerseywonder <work@andyball.info>
2020-04-07 10:30:41 +1000

    Update maps to display cases from gdn cdn feeds

--- src/js/app.js
+++ src/js/app.js
@@ -2,11 +2,23 @@ import loadJson from '../components/load-json/'
 import getURLParams from "./modules/getURLParams"
 import settings from "./settings.json"
 import places from "./places.json"
-import { App } from './modules/applet'
+import { App } from './modules/app'
 
 const state = getURLParams("state") ? getURLParams("state") : "QLD"
 
 var stateData = settings.find( item => item.state === state)
+
+if ( state != null ) {
+
+	loadJson(stateData.url)
+	      .then((data) => {
+	      	new App(data, state, stateData, places)
+	      })
+
+}
+
+/*
+// Version 1
 
 if ( state != null ) {
 
@@ -16,3 +28,5 @@ if ( state != null ) {
 	      })
 
 }
+
+*/
